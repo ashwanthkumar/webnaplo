c62d7e5628ee69a8dd5b85718fdf780d17c8359c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2011-11-17 12:01:50 +0530

    Added documentation to  views/staff/staff.timetable.popup.html.php

--- views/staff/staff.timetable.popup.html.php
+++ views/staff/staff.timetable.popup.html.php
@@ -3,20 +3,24 @@
 	// Get the current user from the application
 	$user = get_user();
 
+	// Get the PDO handle
 	$db = $GLOBALS['db'];
 
 	// Select the list of course profiles the staff takes
 	$courses = $db->select("course_profile", "staff_id = :id", array(":id" => $user->userid));
 	$staff_timetable = $db->run("select hour_of_day, days_of_week, cp_id from timetable where cp_id in (select idcourse_profile from course_profile where staff_id = :sid)", array(":sid" => $user->userid));
 	
+	// Custom Array which contains the timetable of the staff member in a new format
 	$staff_tt_ids = array();
 	foreach($staff_timetable as $st) $staff_tt_ids[$st['days_of_week'] . '_' . $st['hour_of_day']] = $st['cp_id'];
 
-	content_for('body'); 
 
 	// Available time slots in the university
 	// Array index wantedly started from 1 and not from 0. Please preserve it when changing
 	$timeslots = array(1 => "07.30 - 08.40", "08.40 - 09.30", "09.30 - 10.20", "10.20 - 11.10", "10.40 - 11.30", "11.30 - 12.20", "12.20 - 13.10", "13.10 - 14.00", "14.10 - 15.00", "15.00 - 15.50", "16.00 - 16.50", "16.50 - 17.40", "17.40 - 18.30");
+
+	// Marker for the Timetable display
+	content_for('body'); 
 ?>
 <div class="grid_24" >
 	<div class="box_top" style="padding-top: -100px;">
@@ -65,16 +69,24 @@
 <?php
 
 	end_content_for();
-	
+	// End of Timetable display marker
+
+	/**
+	 *	Generates the timetable select boxes for the staff members
+	 *
+	 *	@param	$courses		List of course profiles for the staff members
+	 *	@param	$staff_tt_ids	Timetable of the staff in a custom array
+	 *	@param	$day			Day of the week to generate the list box
+	 *	@param	$hour			Hour of the day to generate the list box
+	 *	@return <select> HTML Element
+	 **/
 	function listCourseProfiles($courses, $staff_tt_ids, $day, $hour) {
 		
-		$user = get_user();
-		$db = $GLOBALS['db'];
-	
 		$r = '<select name="' . $day . '_' . "$hour" . '"><option value="-1">Free </option>';
 		foreach($courses as $c) {
 			$array_key_test = $day . '_' . $hour;
 			
+			// Checking if the slot is already used by the course profile
 			$selected = isset($staff_tt_ids[$array_key_test]) && ($staff_tt_ids[$array_key_test] == $c['idcourse_profile']);
 			
 			if($selected) $selected = " selected = 'selected' "; else $selected = "";
