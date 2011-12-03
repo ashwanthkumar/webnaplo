1686a50eb7075de0971546209017a9aed47da0de
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2011-12-04 00:15:07 +0530

    Added search() to CourseProfile model

--- models/CourseProfile.php
+++ models/CourseProfile.php
@@ -188,4 +188,17 @@ class CourseProfile {
 			return $cprofile;
 		}
 	}
+	
+	/**
+	 *	Search the Model entities in the datastore
+	 *
+	 *	@param	$db			PDOObject
+	 *	@param	$condition	Search Condition 
+	 *	@param	$bind		Array of bound values used in $condition
+	 *
+	 *	@return	Array of Model entities matching the $condition
+	 **/
+	public static function search($db, $condition = '1=1', $bind = array()) {
+		return $db->select("course_profile", $condition, $bind);
+	}	
 }
