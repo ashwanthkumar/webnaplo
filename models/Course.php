4dbc3111d0525ebe3f70960181c29673ce4d69ec
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2011-12-04 21:15:49 +0530

    Update Course model to reflect latest coding conventions

--- models/Course.php
+++ models/Course.php
@@ -7,16 +7,32 @@ class Course{
 	public $credits;
 	public $pgm_id;
 	
+	/**
+	 *	Save the current instance of the courses to the datastore
+	 *
+	 *	@param	$db	PDOObject
+	 *	@return	1 on success, PDOException on failure
+	 **/
 	public function save($db) {
-		return $db->insert("course", array(
-					"idcourse" => $this->idcourse,
+		$r = $db->insert("course", array(
 					"course_code" => $this->coursecode,
 					"course_name" => $this->coursename,
 					"credits" => $this->credits,
 					"programme_id" => $this->pgm_id
 				));
+		
+		if(is_object($r) && get_class($r) == "PDOException") return $r;
+		
+		$this->idcourse = $db->lastInsertId();
+		return $r;
 	}
 	
+	/**
+	 *	Updates the current instance of the Course Model using the $this->idcourse value
+	 *
+	 *	@param	$db		PDOObject
+	 *	@return	1 on success, or PDOException on failure
+	 **/
 	public function update($db) {		
 		return $db->update("course", array(
 				"course_code" => $this->coursecode,
@@ -26,23 +42,65 @@ class Course{
 			), "idcourse = :cid", array(":cid" => $this->idcourse));
 	}
 	
+	/**
+	 *	Deletes an instance of the Model identified by the $cid value
+	 *
+	 *	@param	$cid	Course ID
+	 *	@param	$db		PDOObject
+	 *
+	 *	@return	1 on success, or PDOException on failure
+	 **/
 	public static function Delete($cid, $db) {
 		return $db->delete("course", "course_code = :cc", array(":cc" => $cid));
 	}
 	
+	/**
+	 *	Loads an instance of a new Course from the datastore and returns an instance of the model object.
+	 *
+	 *	@param	$cid	Course ID
+	 *	@param	$db		PDOObject
+	 *
+	 *	@return Course class instance, identified by its Course ID ($cid)
+	 *			FALSE	if the course is not found
+	 *			PDOException in case of an error
+	 **/
+	public static function load($cid, $db) {
+		$r = $db->select("course", "idcourse = :cid", array(":cid" => $cid));
+		
+		if(is_object($r) && get_class($r) == "PDOException") return $r;
+		
+		if(count($r) < 1) return FALSE;
+		else return $r[0];
+	}
+	
+	/**
+	 *	Load and upate an instance of the Course model in the system. Values that are to be modified are passed as Arrays
+	 *
+	 *	@param	$post	Array of values that needs to be changed
+	 *	@param	$db		PDOObject
+	 *
+	 *	@return	See {@link $this->update()} for return values
+	 **/
 	public static function LoadAndUpdate($post, $db) {
 		extract($post);
 
-		$course = new Course;
-		$course->idcourse = $idcourse;
-		$course->credits = $credits;
-		$course->coursecode = $coursecode;
-		$course->coursename = $coursename;
-		$course->pgm_id = $pgm_id;
+		$course = Course::load($idcourse, $db);
+		if(isset($credits))	$course->credits = $credits;
+		if(isset($coursecode))	$course->coursecode = $coursecode;
+		if(isset($coursename))	$course->coursename = $coursename;
+		if(isset($pgm_id))	$course->pgm_id = $pgm_id;
 		
 		return $course->update($db);
 	}
 	
+	/**
+	 *	Load and Save the Course model from the array.
+	 *
+	 *	@param	$courseData	Array representation data of the course
+	 *	@param	$db			PDOObject
+	 *
+	 *	@return	See {@link $this->save()} for return values
+	 **/
 	public static function LoadAndSave($courseData, $db) {
 		extract($courseData);
 
@@ -58,4 +116,18 @@ class Course{
 		
 		return $course->save($db);
 	}
+
+	/**
+	 *	Search the Model entities in the datastore
+	 *
+	 *	@param	$db			PDOObject
+	 *	@param	$condition	Search Condition 
+	 *	@param	$bind		Array of bound values used in $condition
+	 *
+	 *	@return	Array of Model entities matching the $condition
+	 **/
+	public static function search($db, $condition = '1=1', $bind = array()) {
+		return $db->select("course", $condition, $bind);
+	}	
+	
 }
